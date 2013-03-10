23325d65885850591145d461d3833334059dd676
Aria <aria.minaei@gmail.com>
2013-03-10 11:39:51 +0330

    stress test for the scroller

--- scripts/js/demos/metro.js
+++ scripts/js/demos/metro.js
@@ -12,8 +12,5 @@ require(['domReady', 'gesture/handler', 'dommy/dambo', 'dommy/dommy', 'type/scro
   }).addEvent('move-persistent:finish', function(e, id, el, dommy) {
     return dommy.getLazy(id, 'scrolls').finish();
   });
-  dambo.forThe('stress').addEvent('tap', function(e, id, el, dommy) {
-    return el.classList.toggle('stress');
-  });
   return dr(function() {});
 });
